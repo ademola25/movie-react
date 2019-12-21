8465cb8024a498c6208b716f59f95bf704232fda
tijani ademola <tijaniademola25@yahoo.com>
2019-12-21 08:54:00 +0100

    A movie site built with react

---
+++ build/precache-manifest.17f1b0bacc97c323749ad3f7506ac8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c526e79659979ffb2eab735026511ff2",
+    "url": "/movie-react/index.html"
+  },
+  {
+    "revision": "c2abbbe8bfa2eb95c234",
+    "url": "/movie-react/static/css/main.d4164829.chunk.css"
+  },
+  {
+    "revision": "4cd138b916ebddd4f03f",
+    "url": "/movie-react/static/js/2.8bbc015d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movie-react/static/js/2.8bbc015d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2abbbe8bfa2eb95c234",
+    "url": "/movie-react/static/js/main.2c506e20.chunk.js"
+  },
+  {
+    "revision": "c6688354a030c9716224",
+    "url": "/movie-react/static/js/runtime-main.076aef0f.js"
+  }
+]);
